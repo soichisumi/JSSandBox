2846f07e2ef33e42d20ba116f375c65fc33c8236
yoyoyousei <yoyoyousei2016@gmail.com>
2018-04-23 11:17:01 +0900

    promiseByQのテストを追加

--- promiseByQ.js
+++ promiseByQ.js
@@ -4,24 +4,24 @@ module.exports = PromiseByQ;
 
 function PromiseByQ() {}
 
-PromiseByQ.prototype.ret5 = ret5;
-
 let ret5 = () => {
   let deferred = Q.defer();
   deferred.resolve(5);
   return deferred.promise;
 };
 
-PromiseByQ.prototype.showValueLater = showValueLater;
+PromiseByQ.prototype.ret5 = ret5;
 
 let showValueLater = (value) => {
   let deferred = Q.defer();
   setTimeout(() => {
     console.log(value);
-    deferred.resolve();
-  }, timeout);
+    deferred.resolve(value);
+  }, 1000);
   return deferred.promise;
 };
+
+PromiseByQ.prototype.showValueLater = showValueLater;
 
 PromiseByQ.prototype.combined = () => {
   ret5().then(showValueLater);
