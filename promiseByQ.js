75c38adc7d6bc1c8c95852084c075bf2ad91f865
yoyoyousei <yoyoyousei2016@gmail.com>
2018-04-23 10:34:47 +0900

    update promiseByQ

--- promiseByQ.js
+++ promiseByQ.js
@@ -1,8 +1,28 @@
-module.exports = promiseByQ;
+const Q = require('q');
 
-function promiseByQ(){
-}
+module.exports = PromiseByQ;
 
-promiseByQ.prototype.ret5 = ()=>{
+function PromiseByQ() {}
 
-}
+PromiseByQ.prototype.ret5 = ret5;
+
+let ret5 = () => {
+  let deferred = Q.defer();
+  deferred.resolve(5);
+  return deferred.promise;
+};
+
+PromiseByQ.prototype.showValueLater = showValueLater;
+
+let showValueLater = (value) => {
+  let deferred = Q.defer();
+  setTimeout(() => {
+    console.log(value);
+    deferred.resolve();
+  }, timeout);
+  return deferred.promise;
+};
+
+PromiseByQ.prototype.combined = () => {
+  ret5().then(showValueLater);
+};
